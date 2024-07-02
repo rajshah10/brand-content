e128903e89beb70886a167a4198f194d814989c1
rajshah1606 <raj.shah@budhanatech.com>
2024-07-02 18:15:41 +0530

    Onboarding component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import ContentCreatorFlow from "./components/ContentCreatorFlow";
 
 function App() {
   const [loginToggle, setLoginToggle] = useState(false);
-  const [selectedOption, setSelectedOption] = useState('contentCreator');
+  const [selectedOption, setSelectedOption] = useState('');
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (selectedOption === "contentCreator") {
+      setFormSubmitted(true);
+    }
   };
   const handleRegister = () => {
     setLoginToggle(!loginToggle);
@@ -26,31 +31,35 @@ function App() {
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Choose one
+            {formSubmitted ? "Content Creator Onboarding" : "Choose one"}
           </h2>
         </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form onSubmit={handleSubmit} className="space-y-6">
-            <div className="grid grid-cols-2 text-center gap-2">
-              <div
-                className={`border py-4 rounded-md cursor-pointer ${selectedOption === 'contentCreator' ? 'border-slate-400' : 'border-slate-200'
-                  }`}
-                onClick={() => handleOptionClick('contentCreator')}
-              >
-                <span>Content Creator</span>
+        {
+          selectedOption === "contentCreator" && <></>
+        }
+        {
+          !formSubmitted && <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form onSubmit={handleSubmit} className="space-y-6">
+              <div className="grid grid-cols-2 text-center gap-2">
+                <div
+                  className={`border py-4 rounded-md cursor-pointer ${selectedOption === 'contentCreator' ? 'border-slate-400' : 'border-slate-200'
+                    }`}
+                  onClick={() => handleOptionClick('contentCreator')}
+                >
+                  <span>Content Creator</span>
+                </div>
+                <div
+                  className={`border py-4 rounded-md cursor-pointer ${selectedOption === 'brand' ? 'border-slate-400' : 'border-slate-200'
+                    }`}
+                  onClick={() => handleOptionClick('brand')}
+                >
+                  <span>Brand</span>
+                </div>
               </div>
-              <div
-                className={`border py-4 rounded-md cursor-pointer ${selectedOption === 'brand' ? 'border-slate-400' : 'border-slate-200'
-                  }`}
-                onClick={() => handleOptionClick('brand')}
-              >
-                <span>Brand</span>
-              </div>
-            </div>
-            {loginToggle && (
-              <>
-                {/* <div>
+              {loginToggle && (
+                <>
+                  {/* <div>
                   <label htmlFor="role" className="block text-sm font-medium leading-6 text-gray-900">Select Role</label>
                   <div className="mt-2">
                     <select id="role" name="role" className="block w-full rounded-md border-0 py-2 px-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300  sm:text-sm sm:leading-6">
@@ -59,72 +68,76 @@ function App() {
                     </select>
                   </div>
                 </div> */}
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Email address
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      autoComplete="email"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 px-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-
-                <div>
-                  <div className="flex items-center justify-between">
+                  <div>
                     <label
-                      htmlFor="password"
+                      htmlFor="email"
                       className="block text-sm font-medium leading-6 text-gray-900"
                     >
-                      Password
+                      Email address
                     </label>
-                    <div className="text-sm">
-                      <a
-                        href="#"
-                        className="font-semibold text-indigo-600 hover:text-indigo-500"
-                      >
-                        Forgot password?
-                      </a>
+                    <div className="mt-2">
+                      <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 px-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
+                      />
                     </div>
                   </div>
-                  <div className="mt-2">
-                    <input
-                      id="password"
-                      name="password"
-                      type="password"
-                      autoComplete="current-password"
-                      required
-                      className="block px-1 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
-                    />
+
+                  <div>
+                    <div className="flex items-center justify-between">
+                      <label
+                        htmlFor="password"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Password
+                      </label>
+                      <div className="text-sm">
+                        <a
+                          href="#"
+                          className="font-semibold text-indigo-600 hover:text-indigo-500"
+                        >
+                          Forgot password?
+                        </a>
+                      </div>
+                    </div>
+                    <div className="mt-2">
+                      <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        autoComplete="current-password"
+                        required
+                        className="block px-1 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
+                      />
+                    </div>
                   </div>
-                </div>
-              </>
-            )}
+                </>
+              )}
 
-            <div>
-              <button className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                Next
-              </button>
-            </div>
-          </form>
+              <div>
+                <button className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                  Next
+                </button>
+              </div>
+            </form>
 
-          <p className="mt-4 text-sm text-gray-500">
-            <a
-              onClick={handleRegister}
-              className="font-semibold cursor-pointer leading-6 text-indigo-600 hover:text-indigo-500"
-            >
-              {!loginToggle ? "Login" : "Back"}
-            </a>
-          </p>
-        </div>
+            <p className="mt-4 text-sm text-gray-500">
+              <a
+                onClick={handleRegister}
+                className="font-semibold cursor-pointer leading-6 text-indigo-600 hover:text-indigo-500"
+              >
+                {!loginToggle ? "Login" : "Back"}
+              </a>
+            </p>
+          </div>
+        }
+        {
+          formSubmitted && <ContentCreatorFlow />
+        }
       </div>
       <div
         className="hidden md:flex w-full md:w-3/5 bg-cover bg-center"
---
+++ src/components/ContentCreatorFlow.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContentCreatorFlow = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ContentCreatorFlow
