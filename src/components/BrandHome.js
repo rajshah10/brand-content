b45e54adf4272ea11b4af7b831e65a370c422334
yash patil <patilyash445@gmail.com>
2024-07-03 19:29:31 +0530

    cahnges in brands

--- src/components/BrandHome.js
+++ src/components/BrandHome.js
@@ -44,8 +44,8 @@ const BrandHome = () => {
 
     const StyledBadge = styled(Badge)(({ theme }) => ({
         '& .MuiBadge-badge': {
-        //    background:"white",
-        //    borderRadius:"50%"
+            //    background:"white",
+            //    borderRadius:"50%"
         },
     }));
     return (
@@ -91,7 +91,7 @@ const BrandHome = () => {
                                 key={influencer.id}
                                 className="bg-white shadow-md rounded-lg p-4 flex flex-col space-y-4 hover:scale-105 transform transition duration-300 ease-in-out"
                             >
-                                <div className="flex flex-col md:flex-row md:items-start">
+                                <div className="flex flex-col md:flex-row sm:items-center md:justify-center lg:items-start lg:justify-start" style={{alignItems:"center"}}>
                                     <div className="relative">
                                         <StyledBadge
                                             overlap="circular"
@@ -105,18 +105,18 @@ const BrandHome = () => {
                                                 <HelpOutlineIcon className="text-orange-400" />
                                             )}
                                         >
-                                            <Avatar className="brand_avatar rounded-full" src={influencer.image} alt={influencer.name} />
+                                            <Avatar className="brand_avatar w-40 h-40 rounded-full" src={influencer.image} alt={influencer.name} />
                                         </StyledBadge>
                                     </div>
-                                    <div className="flex-1 ml-4 mt-4 md:mt-0 md:ml-4">
-                                        <h3 className="text-lg font-medium text-black-600 flex items-center">
+                                    <div className="flex-1 ml-4 mt-4 md:mt-0 md:ml-4 md:text-center lg:text-left">
+                                        <h3 className="text-lg font-medium text-black-600 flex items-center justify-center md:justify-center lg:justify-start">
                                             {influencer.name}
                                             {influencer.verified && (
                                                 <span className="ml-1 text-xs text-blue-500"><VerifiedIcon /></span>
                                             )}
                                         </h3>
                                         <p className="text-gray-400">{influencer.description}</p>
-                                        <div className="flex gap-1 mt-2">
+                                        <div className="flex gap-1 mt-2 justify-center md:justify-center lg:justify-start">
                                             {influencer.interests.map((interest, index) => (
                                                 <div key={index} className="bg-gray-100 px-2 py-1 rounded-md text-xs text-gray-600">
                                                     {interest}
@@ -124,22 +124,23 @@ const BrandHome = () => {
                                             ))}
                                         </div>
                                     </div>
-                                    <div className="flex gap-4 flex-col md:flex-row md:gap-4 mt-4 md:mt-0">
+                                    <div className="flex gap-4 flex-col md:flex-row md:gap-4 mt-4 md:mt-0 md:justify-center lg:justify-start">
                                         {/* <Button variant="outlined" color="success" startIcon={<Done />}>
-                                            In selection
-                                        </Button>
-                                        <div className="grid bg-gray-100 rounded px-1">
-                                            <div className="flex items-center justify-between gap-4">
-                                                <div className="text-gray-500 text-xs">Price from a blogger</div>
-                                                <div className="text-gray-500 barnd_edit_icon"><Edit className="text-gray-500 text-sm/[5px]"/></div>
-                                            </div>
-                                            <div className="text-gray-500 text-xs">$2050</div>
-                                        </div> */}
+            In selection
+        </Button>
+        <div className="grid bg-gray-100 rounded px-1">
+            <div className="flex items-center justify-between gap-4">
+                <div className="text-gray-500 text-xs">Price from a blogger</div>
+                <div className="text-gray-500 barnd_edit_icon"><Edit className="text-gray-500 text-sm/[5px]"/></div>
+            </div>
+            <div className="text-gray-500 text-xs">$2050</div>
+        </div> */}
                                         <Button variant="contained" color="error" startIcon={<ShowChartIcon />}>
                                             Statistics
                                         </Button>
                                     </div>
                                 </div>
+
 
                                 <div className="flex flex-wrap justify-center bg-gray-100 gap-4 text-sm text-gray-600 mt-4">
                                     <div className="flex-1 flex flex-col items-center border-r border-black-600 p-2 rounded-md">
