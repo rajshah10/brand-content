e7969901a95a46924e1f59b6a1e82eb469c4b430
rajshah1606 <raj.shah@budhanatech.com>
2024-07-03 13:55:37 +0530

    Added Profile

--- src/components/Influencers.js
+++ src/components/Influencers.js
@@ -21,10 +21,41 @@ const Influencers = () => {
         }
         return str.slice(0, num) + '...';
     };
+    const campaigns = [
+        {
+            title: "Summer Sale 2023, New Product launch - Summer 2023, back to School Sale 2023",
+            description: "Nike is thrilled to announce the launch of our new product line for Summer 2023! This season, we’re introducing innovative designs and cutting-edge technology to enhance your performance and style. Our new collection features advanced materials for ultimate comfort and durability, along with fresh, vibrant colors that capture the spirit of summer. From high-performance running shoes to versatile training gear and stylish athleisure wear, our Summer 2023 launch has something for everyone. Be among the first to experience the latest in sportswear innovation and take your game to the next level with Nike",
+            platform: "Instagram",
+            type: "Paid",
+            timeAgo: "1 hr ago",
+            proposals: 2,
+            creators: 2,
+            hired: 2,
+            images: [
+                "https://hips.hearstapps.com/hmg-prod/images/index-bomber-65a839208f31a.jpg?resize=2048:*",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf3Ehqyb83E1sTv48tNl86cfKMRyy0YsTRRA&s"
+            ]
+        },
+        {
+            title: "Summer Sale 2023, New Product launch - Summer 2023, back to School Sale 2023",
+            description: "Nike is thrilled to announce the launch of our new product line for Summer 2023! This season, we’re introducing innovative designs and cutting-edge technology to enhance your performance and style. Our new collection features advanced materials for ultimate comfort and durability, along with fresh, vibrant colors that capture the spirit of summer. From high-performance running shoes to versatile training gear and stylish athleisure wear, our Summer 2023 launch has something for everyone. Be among the first to experience the latest in sportswear innovation and take your game to the next level with Nike",
+            platform: "Instagram",
+            type: "Paid",
+            timeAgo: "1 hr ago",
+            proposals: 2,
+            creators: 2,
+            hired: 2,
+            images: [
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSed7XOOAyH0ZNkdaMt13e8mgyo_GiVRMnug&s",
+                "https://pictures-ethiopia.jijistatic.com/2274267_NjIwLTYyMC04N2QzOTNkYTMw.webp"
+            ]
+        }
+    ];
+
     return (
         <>
             <div>
-                <MenuComponent open={open} anchorEl={anchorEl} handleClose={handleClose}/>
+                <MenuComponent open={open} anchorEl={anchorEl} handleClose={handleClose} />
                 <div className=" bg-white shadow-sm  w-full px-3 sm:px-4 md:px-8 lg:px-8 py-3 flex justify-between">
                     <div>
                         <img
@@ -194,115 +225,55 @@ const Influencers = () => {
                             </select>
                         </div>
                     </div>
-                    <div className="border border-slate-200 my-5 rounded-md bg-slate-100">
-                        <div className="m-2 p-2 font-medium bg-white rounded-md grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-                            <div className="flex flex-col">
-                                <span>
-                                    Summer Sale 2023, New Product launch - Summer 2023, back to
-                                    School Sale 2023
-                                </span>
-                                <span className="text-sm mt-2 flex items-center gap-1">
-                                    <InstagramIcon style={{ fontSize: "13px", color: "maroon" }} />
-                                    Instagram .{" "}
-                                    <span className="text-slate-500 flex items-center gap-1">
-                                        <PaidOutlinedIcon
-                                            style={{ fontSize: "13px", color: "slate" }}
-                                        />
-                                        Paid
-                                    </span>{" "}
-                                    . <span className="text-slate-500">1 hr ago</span>
-                                </span>
-                            </div>
-                            <div className="flex gap-2  mt-3 md:mt-0 lg:mt-0">
-                                <div className="flex flex-col items-center border border-slate-300 p-2 w-full rounded-md h-16">
-                                    <span>02</span>
-                                    <span className="text-sm  text-slate-500">Proposals</span>
+                    {campaigns.map((campaign, index) => (
+                        <div key={index} className="border border-slate-200 my-5 rounded-md bg-slate-100">
+                            <div className="m-2 p-2 font-medium bg-white rounded-md grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+                                <div className="flex flex-col">
+                                    <span>{campaign.title}</span>
+                                    <span className="text-sm mt-2 flex items-center gap-1">
+                                        <InstagramIcon style={{ fontSize: "13px", color: "maroon" }} />
+                                        {campaign.platform} .{" "}
+                                        <span className="text-slate-500 flex items-center gap-1">
+                                            <PaidOutlinedIcon style={{ fontSize: "13px", color: "slate" }} />
+                                            {campaign.type}
+                                        </span>{" "}
+                                        . <span className="text-slate-500">{campaign.timeAgo}</span>
+                                    </span>
                                 </div>
-                                <div className="flex flex-col items-center border border-slate-300 p-2 rounded-md w-full h-16">
-                                    <span>02</span>
-                                    <span className="text-sm  text-slate-500">Creators</span>
-                                </div>
-                                <div className="flex flex-col items-center border border-slate-300 p-2 rounded-md w-full h-16">
-                                    <span>02</span>
-                                    <span className="text-sm  text-slate-500">Hired</span>
+                                <div className="flex gap-2 mt-3 md:mt-0 lg:mt-0">
+                                    <div className="flex flex-col items-center border border-slate-300 p-2 w-full rounded-md h-16">
+                                        <span>{campaign.proposals}</span>
+                                        <span className="text-sm text-slate-500">Proposals</span>
+                                    </div>
+                                    <div className="flex flex-col items-center border border-slate-300 p-2 rounded-md w-full h-16">
+                                        <span>{campaign.creators}</span>
+                                        <span className="text-sm text-slate-500">Creators</span>
+                                    </div>
+                                    <div className="flex flex-col items-center border border-slate-300 p-2 rounded-md w-full h-16">
+                                        <span>{campaign.hired}</span>
+                                        <span className="text-sm text-slate-500">Hired</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="m-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3">
-                            <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-2">
-                                <img src="https://hips.hearstapps.com/hmg-prod/images/index-bomber-65a839208f31a.jpg?resize=2048:*" className="rounded-md h-32 w-full object-cover" />
-                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf3Ehqyb83E1sTv48tNl86cfKMRyy0YsTRRA&s" className="rounded-md h-32 w-full object-cover" />
-                            </div>
-                            <div className="w-full flex flex-col justify-between">
-                                <h6 className="text-sm">
-                                    {truncateString("Nike is thrilled to announce the launch of our new product line for Summer 2023! This season, we’re introducing innovative designs and cutting-edge technology to enhance your performance and style. Our new collection features advanced materials for ultimate comfort and durability, along with fresh, vibrant colors that capture the spirit of summer. From high-performance running shoes to versatile training gear and stylish athleisure wear, our Summer 2023 launch has something for everyone. Be among the first to experience the latest in sportswear innovation and take your game to the next level with Nike", 180)}
-                                </h6>
-                                <div className="flex justify-end ">
-                                    <button className="rounded-md bg-indigo-600 px-6 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                        Apply
-                                    </button>
+                            <div className="m-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3">
+                                <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-2">
+                                    {campaign.images.map((image, idx) => (
+                                        <img key={idx} src={image} className="rounded-md h-32 w-full object-cover" />
+                                    ))}
                                 </div>
-
-                            </div>
-                        </div>
-                    </div>
-
-
-
-
-
-                    {/* Second */}
-                    <div className="border border-slate-200 my-5 rounded-md bg-slate-100">
-                        <div className="m-2 p-2 font-medium bg-white rounded-md grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-                            <div className="flex flex-col">
-                                <span>
-                                    Summer Sale 2023, New Product launch - Summer 2023, back to
-                                    School Sale 2023
-                                </span>
-                                <span className="text-sm mt-2 flex items-center gap-1">
-                                    <InstagramIcon style={{ fontSize: "13px", color: "maroon" }} />
-                                    Instagram .{" "}
-                                    <span className="text-slate-500 flex items-center gap-1">
-                                        <PaidOutlinedIcon
-                                            style={{ fontSize: "13px", color: "slate" }}
-                                        />
-                                        Paid
-                                    </span>{" "}
-                                    . <span className="text-slate-500">1 hr ago</span>
-                                </span>
-                            </div>
-                            <div className="flex gap-2  mt-3 md:mt-0 lg:mt-0">
-                                <div className="flex flex-col items-center border border-slate-300 p-2 w-full rounded-md h-16">
-                                    <span>02</span>
-                                    <span className="text-sm  text-slate-500">Proposals</span>
-                                </div>
-                                <div className="flex flex-col items-center border border-slate-300 p-2 rounded-md w-full h-16">
-                                    <span>02</span>
-                                    <span className="text-sm  text-slate-500">Creators</span>
-                                </div>
-                                <div className="flex flex-col items-center border border-slate-300 p-2 rounded-md w-full h-16">
-                                    <span>02</span>
-                                    <span className="text-sm  text-slate-500">Hired</span>
+                                <div className="w-full flex flex-col justify-between">
+                                    <h6 className="text-sm">
+                                        {truncateString(campaign.description, 180)}
+                                    </h6>
+                                    <div className="flex justify-end">
+                                        <button className="rounded-md bg-indigo-600 px-6 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                            Apply
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="m-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3">
-                            <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-2">
-                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSed7XOOAyH0ZNkdaMt13e8mgyo_GiVRMnug&s" className="rounded-md h-32 w-full object-cover" />
-                                <img src="https://pictures-ethiopia.jijistatic.com/2274267_NjIwLTYyMC04N2QzOTNkYTMw.webp" className="rounded-md h-32 w-full object-cover" />
-                            </div>
-                            <div className="w-full flex flex-col justify-between">
-                                <h6 className="text-sm">
-                                    {truncateString("Nike is thrilled to announce the launch of our new product line for Summer 2023! This season, we’re introducing innovative designs and cutting-edge technology to enhance your performance and style. Our new collection features advanced materials for ultimate comfort and durability, along with fresh, vibrant colors that capture the spirit of summer. From high-performance running shoes to versatile training gear and stylish athleisure wear, our Summer 2023 launch has something for everyone. Be among the first to experience the latest in sportswear innovation and take your game to the next level with Nike", 180)}
-                                </h6>
-                                <div className="flex justify-end ">
-                                    <button className="rounded-md bg-indigo-600 px-6 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                        Apply
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </Container>
         </>
