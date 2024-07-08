a7508d2bf32e615926716bd05eb525e28641b643
rajshah1606 <raj.shah@budhanatech.com>
2024-07-08 13:01:49 +0530

    changesof Navbar logo

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { useNavigate } from 'react-router';
 
 const Header = (props) => {
     const { handleClick } = props;
-
+    const navigate = useNavigate()
     return (
         <div className="bg-white shadow-sm w-full px-3 sm:px-4 md:px-8 lg:px-8 flex justify-between items-center">
             <div>
                 <img
-                    className="h-24"
+                    className="h-24 cursor-pointer"
                     src={require("../../assets/images/Logo.png")}
                     alt="Your Company"
+                    onClick={() => navigate('/')}
                 />
             </div>
             <div className="relative">
