a1ea177504ca3f61a01e559a12a9956610e3a5b9
rajshah1606 <raj.shah@budhanatech.com>
2024-07-09 18:37:23 +0530

    changes

--- src/components/Influencers.js
+++ src/components/Influencers.js
@@ -114,7 +114,7 @@ const Influencers = () => {
 
 
                 <div class="bg-white py-24 sm:py-32">
-                    <div class="mx-auto px-6">
+                    <div class="mx-auto">
                         <div class="mx-auto max-w-2xl sm:text-center">
                             <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Join Equellence as Content Creator</h2>
                             <p class="mt-6 text-lg leading-8 text-gray-600">Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et quasi iusto modi velit ut non voluptas in. Explicabo id ut laborum.</p>
