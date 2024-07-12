bd732bc9fbf79af6e89b03b1daf4c7e516dae803
Raj Shah <shahrajnew135421@gmail.com>
2024-07-12 12:54:19 +0530

    changes

--- src/components/BrandHome.js
+++ src/components/BrandHome.js
@@ -98,18 +98,50 @@ const BrandHome = () => {
                 <MenuComponent open={openMenu} anchorEl={anchorEl} handleClose={handleClose} />
                 <Header handleClick={handleClick} />
             </div>
-            <div className="relative w-full h-96 bg-cover bg-center" style={{ backgroundImage: `url(https://images.pexels.com/photos/1598508/pexels-photo-1598508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)` }}>
-                <div className="absolute flex flex-col inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-                    <h1 className="text-white text-4xl font-bold text-center">
-                        Brands
-                    </h1>
-                    <div className="py-4 px-6 text-slate-300">
-                        <p className="text-lg">
-                            Insert your description of the content creator here. This could include details about their work, achievements, or any other relevant information.
-                        </p>
+            <div className="relative pt-16 pb-32 flex content-center items-center justify-center"
+                    style={{
+                        minHeight: "58vh"
+                    }}>
+                    <div className="absolute top-0 w-full h-full bg-center bg-cover"
+                        style={{
+                            backgroundImage: `url('https://images.unsplash.com/photo-1590102426080-52ecff3affeb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`
+                        }}>
+                        <span id="blackOverlay" className="w-full h-full absolute opacity-75 bg-black"></span>
+                    </div>
+                    <div className="container relative mx-auto">
+                        <div className="items-center flex flex-wrap">
+                            <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+                                <div className="pr-12">
+                                    <h1 className="text-white font-semibold text-5xl">
+                                        Brands
+                                    </h1>
+                                    <p className="mt-4 text-lg text-gray-300">
+                                        Learn more about our journey and our team.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
+                        style={{ height: "70px" }}
+                    >
+                        <svg
+                            className="absolute bottom-0 overflow-hidden"
+                            xmlns="http://www.w3.org/2000/svg"
+                            preserveAspectRatio="none"
+                            version="1.1"
+                            viewBox="0 0 2560 100"
+                            x="0"
+                            y="0"
+                        >
+                            <polygon
+                                className="text-white fill-current"
+                                points="2560 0 2560 100 0 100"
+                            ></polygon>
+                        </svg>
                     </div>
                 </div>
-            </div>
 
             <Container className="mt-10 flex">
                 <div className='bg-slate-50 p-3 text-gray-600 mx-4'>
--- src/components/Influencers.js
+++ src/components/Influencers.js
@@ -96,18 +96,49 @@ const Influencers = () => {
                 <MenuComponent open={open} anchorEl={anchorEl} handleClose={handleClose} />
                 <Header handleClick={handleClick} />
             </div>
-            <div className="relative w-full h-96 bg-cover bg-center" style={{ backgroundImage: `url(https://images.pexels.com/photos/1598508/pexels-photo-1598508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)` }}>
-                <div className="absolute  flex-col inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-                    <h1 className="text-white text-4xl font-bold text-center">
-                        Content Creator
-                    </h1>
-                    <div className="py-4 px-6 text-slate-300">
-                        <p className="text-lg">
-                            Insert your description of the content creator here. This could include details about their work, achievements, or any other relevant information.
-                        </p>
+            <div className="relative pt-16 pb-32 flex content-center items-center justify-center"
+                style={{
+                    minHeight: "58vh"
+                }}>
+                <div className="absolute top-0 w-full h-full  bg-cover"
+                    style={{
+                        backgroundImage: `url('https://plus.unsplash.com/premium_photo-1684581969889-67d386781d37?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`
+                    }}>
+                    <span id="blackOverlay" className="w-full h-full absolute opacity-75 bg-black"></span>
+                </div>
+                <div className="container relative mx-auto">
+                    <div className="items-center flex flex-wrap">
+                        <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+                            <div className="pr-12">
+                                <h1 className="text-white font-semibold text-5xl">
+                                    Influencers
+                                </h1>
+                                <p className="mt-4 text-lg text-gray-300">
+                                    Learn more about our journey and our team.
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
+                <div
+                    className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
+                    style={{ height: "70px" }}
+                >
+                    <svg
+                        className="absolute bottom-0 overflow-hidden"
+                        xmlns="http://www.w3.org/2000/svg"
+                        preserveAspectRatio="none"
+                        version="1.1"
+                        viewBox="0 0 2560 100"
+                        x="0"
+                        y="0"
+                    >
+                        <polygon
+                            className="text-white fill-current"
+                            points="2560 0 2560 100 0 100"
+                        ></polygon>
+                    </svg>
+                </div>
             </div>
 
             <Container>
