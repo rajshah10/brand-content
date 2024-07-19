84671968a5be191e13d1e5dfccaaf4ad94f67a0c
yash patil <patilyash445@gmail.com>
2024-07-19 15:07:39 +0530

    api integration for create campaign

--- src/components/CampaignCreation.js
+++ src/components/CampaignCreation.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import MenuComponent from "./common/MenuComponent";
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import { Container } from '@mui/material';
+import { CircularProgress, Container } from '@mui/material';
 import Header from './common/Header';
 import toast, { Toaster } from 'react-hot-toast';
 
@@ -10,6 +10,8 @@ import axios from 'axios';
 const CampaignCreation = () => {
     const [anchorEl, setAnchorEl] = useState(null);
     const [buttonColor, setButtonColor] = useState('bg-indigo-600');
+    const [loading, setLoading] = useState(false);
+
     const openMenu = Boolean(anchorEl);
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
@@ -26,6 +28,8 @@ const CampaignCreation = () => {
         deliverables: '',
         deadlines: '',
         compensation: '',
+        COEmail: '',
+        brand: '',
         // createdDateTime: new Date().toISOString()
     });
 
@@ -38,24 +42,29 @@ const CampaignCreation = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setLoading(true);
         // Handle the campaign data submission
         try {
-
-
             const response = await axios.post('http://localhost:5001/api/campaign', campaign);
             // console.log("response",response)
             if (response.status === 200) {
                 toast.success('Form submitted successfully!');
                 // alert("successfully submitted..!")
+                setLoading(false);
             }
 
         } catch (error) {
             console.error('Error submitting the form:', error);
+            setLoading(false);
         }
     };
 
     return (
         <>
+          <Toaster
+                position="top-right"
+                reverseOrder={false}
+            />
             <div>
                 <MenuComponent open={openMenu} anchorEl={anchorEl} handleClose={handleClose} />
                 <Header handleClick={handleClick} />
@@ -63,53 +72,27 @@ const CampaignCreation = () => {
             <div className="mx-auto px-4 sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
 
                 <div className="my-12">
-                    <h2 class="font-bold text-lg">Create New Campaign</h2>
-                    <p class="mt-1 text-sm leading-6 text-gray-600">This information will be displayed publicly so be careful what you share.</p>
+                    <h2 className="font-bold text-lg">Create New Campaign</h2>
+                    <p className="mt-1 text-sm leading-6 text-gray-600">This information will be displayed publicly so be careful what you share.</p>
 
                     <form onSubmit={handleSubmit} className="space-y-12 mt-10">
                         <div className="">
                             <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                                <div class="sm:col-span-3">
-                                    <label for="companyName" class="block text-sm font-medium leading-6 text-gray-900">Company Name</label>
-                                    <div class="mt-2">
-                                        <input type="text" name="companyName" id="first-name" autocomplete="given-name" value={campaign.companyName}
-                                            onChange={handleChange} class="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 outline-none  sm:text-sm sm:leading-6 px-2" />
+                                <div className="sm:col-span-3">
+                                    <label htmlFor="companyName" className="block text-sm font-medium leading-6 text-gray-900">Company Name</label>
+                                    <div className="mt-2">
+                                        <input type="text" name="companyName" id="first-name" autoComplete="given-name" value={campaign.companyName}
+                                            onChange={handleChange} className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 outline-none  sm:text-sm sm:leading-6 px-2" />
                                     </div>
                                 </div>
 
-                                <div class="sm:col-span-3">
-                                    <label for="campaignTitle" class="block text-sm font-medium leading-6 text-gray-900">Campaign Title</label>
-                                    <div class="mt-2">
-                                        <input type="text" name="campaignTitle" id="last-name" autocomplete="family-name" value={campaign.campaignTitle}
-                                            onChange={handleChange} class="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 outline-none  sm:text-sm sm:leading-6 px-2" />
-                                    </div>
-                                </div>
-                                {/* <div className="sm:col-span-3">
-                                    <label htmlFor="companyName" className="block text-sm font-medium leading-6 text-gray-900">Company Name</label>
-                                    <div className="mt-2 flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 sm:max-w-md">
-                                        <input
-                                            type="text"
-                                            name="companyName"
-                                            id="companyName"
-                                            value={campaign.companyName}
-                                            onChange={handleChange}
-                                            className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6 outline-none"
-                                        />
-                                    </div>
-                                </div>
                                 <div className="sm:col-span-3">
                                     <label htmlFor="campaignTitle" className="block text-sm font-medium leading-6 text-gray-900">Campaign Title</label>
-                                    <div className="mt-2 flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 sm:max-w-md">
-                                        <input
-                                            type="text"
-                                            name="campaignTitle"
-                                            id="campaignTitle"
-                                            value={campaign.campaignTitle}
-                                            onChange={handleChange}
-                                            className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6 outline-none"
-                                        />
+                                    <div className="mt-2">
+                                        <input type="text" name="campaignTitle" id="last-name" autoComplete="family-name" value={campaign.campaignTitle}
+                                            onChange={handleChange} className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 outline-none  sm:text-sm sm:leading-6 px-2" />
                                     </div>
-                                </div> */}
+                                </div>
                                 <div className="col-span-full">
                                     <label htmlFor="campaignDescription" className="block text-sm font-medium leading-6 text-gray-900">Campaign Description</label>
                                     <div className="mt-2">
@@ -175,6 +158,32 @@ const CampaignCreation = () => {
                                         />
                                     </div>
                                 </div>
+                                <div className="col-span-full">
+                                    <label htmlFor="brand" className="block text-sm font-medium leading-6 text-gray-900">Brand</label>
+                                    <div className="mt-2">
+                                        <input
+                                            type="text"
+                                            name="brand"
+                                            id="brand"
+                                            value={campaign.brand}
+                                            onChange={handleChange}
+                                            className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 outline-none"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="col-span-full">
+                                    <label htmlFor="COEmail" className="block text-sm font-medium leading-6 text-gray-900">Campaign Owner Email</label>
+                                    <div className="mt-2">
+                                        <input
+                                            type="text"
+                                            name="COEmail"
+                                            id="COEmail"
+                                            value={campaign.COEmail}
+                                            onChange={handleChange}
+                                            className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 outline-none"
+                                        />
+                                    </div>
+                                </div>
                                 {/* <div className="col-span-full">
                                     <label htmlFor="createdDateTime" className="block text-sm font-medium leading-6 text-gray-900">Created DateTime</label>
                                     <div className="mt-2">
@@ -191,9 +200,10 @@ const CampaignCreation = () => {
                             </div>
                         </div>
                         <button
-                            className={` bg-indigo-600 rounded-md px-6 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:${buttonColor} focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
+                            type="submit"
+                            className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-100"
                         >
-                            Create Campaign
+                            {loading ? <CircularProgress size={"22px"} sx={{ color: "white" }} />: " Create Campaign"}
                         </button>
 
                     </form>
